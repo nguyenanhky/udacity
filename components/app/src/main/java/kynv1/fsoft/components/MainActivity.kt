d3bda1a38db7d70daca4f11b794499db907df772
nguyenanhky <chienky2016@gmail.com>
2023-10-07 15:35:06 +0700

    snack

--- components/app/src/main/java/kynv1/fsoft/components/MainActivity.kt
+++ components/app/src/main/java/kynv1/fsoft/components/MainActivity.kt
@@ -1,11 +1,35 @@
 package kynv1.fsoft.components
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnAction:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnAction = findViewById(R.id.btn_action)
+        btnAction.setOnClickListener {
+            val message = "Chào mừng bạn đến với Snackbar!"
+            val duration = Snackbar.LENGTH_LONG
+
+            val snackbar = Snackbar.make(
+                it, // View cha mà Snackbar sẽ hiển thị trên đó
+                message, // Nội dung thông báo
+                duration // Thời gian hiển thị (có thể là LENGTH_SHORT hoặc LENGTH_LONG)
+            )
+
+            snackbar.setAction("Đóng") {
+                // Xử lý khi người dùng bấm nút "Đóng"
+                snackbar.dismiss()
+            }
+
+            snackbar.show()
+        }
+
     }
 }
